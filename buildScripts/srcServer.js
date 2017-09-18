1fd9e5c49d4b5851effbd248be31a9013078a2e4
marcstevens92 <mstevens@wssib.com>
2017-09-18 10:15:20 -0500

    Module 9

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,6 +4,8 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+/* eslint-disable no-console */
+
 const port =9292;
 const app = express();
 const compiler = webpack(config);
@@ -15,11 +17,11 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
-;})
+});
 
 app.listen(port, function(err){
   if(err){
-    consol.log(err);
+    console.log(err);
   } else {
     open('http://localhost:' + port);
   }
